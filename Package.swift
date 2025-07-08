46d8fef8535e94ccd855d6179e403aa1936406c0
Nate Sinnott <165043290+natesinnott@users.noreply.github.com>
2025-07-09 00:48:23 +0100

    Update Package.swift
    Add tvOS platform, update swift tools version
    
    Signed-off-by: Nate Sinnott <165043290+natesinnott@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "WireGuardKit",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
